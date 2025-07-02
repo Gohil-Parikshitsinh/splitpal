73293eaba6959791d0232d626baf5465838e2532
Gohil-Parikshitsinh <parikshitsinhgohil03@gmail.com>
2025-07-02 22:19:34 +0530

    Backend Setup

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,9 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import { ConvexClientProvider } from "@/components/ConvexClientProvider";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "SplitPal",
@@ -15,11 +16,11 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="icon" href="/logos/logo3.png" sizes="any" />
       </head>
-      <body
-        className={`${inter.className} `}
-      >
-        <Header/>
-        <main className="min-h-screen">{children}</main>
+      <body className={`${inter.className} `}>
+        <ConvexClientProvider>
+          <Header />
+          <main className="min-h-screen">{children}</main>
+        </ConvexClientProvider>
       </body>
     </html>
   );
