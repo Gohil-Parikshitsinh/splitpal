56336d4dc898bedfb2213c50404d11dc79860eae
Gohil-Parikshitsinh <parikshitsinhgohil03@gmail.com>
2025-07-04 18:55:56 +0530

    Connecting Convex with clerk

--- components/ConvexClientProvider.jsx
+++ components/ConvexClientProvider.jsx
@@ -1,16 +1,17 @@
 "use client";
 
+import { useAuth } from "@clerk/nextjs";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
 
 export function ConvexClientProvider({ children }) {
   return (
-    <ConvexProvider
+    <ConvexProviderWithClerk
       client={convex}
-      //    useAuth={}
+         useAuth={useAuth}
     >
       {children}
-    </ConvexProvider>
+    </ConvexProviderWithClerk>
   );
 }
