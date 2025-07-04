83c17d3abc2af05300bace858b72e08c0d097a17
Gohil-Parikshitsinh <parikshitsinhgohil03@gmail.com>
2025-07-04 19:55:59 +0530

    Header Component

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>Hello from GohilCoder
+    <div className="pt-16">Hello from GohilCoder
       <br/>
       <Button variant={'destructive'}>Code</Button>
     </div>
