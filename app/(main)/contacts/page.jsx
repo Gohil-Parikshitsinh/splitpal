251ad71673450efd60a39823a1d381ff4324aac6
Gohil-Parikshitsinh <parikshitsinhgohil03@gmail.com>
2025-07-11 07:19:32 +0530

    Custom Hook for fetching Data

--- app/(main)/contacts/page.jsx
+++ app/(main)/contacts/page.jsx
@@ -1,8 +1,22 @@
 "use client"
 
+import { api } from '@/convex/_generated/api'
+import { useConvexQuery } from '@/hooks/use-convex-query'
 import React from 'react'
+import { BarLoader } from 'react-spinners'
 
 const ConatctPage = () => {
+  const {data, isLoading} = useConvexQuery(api.contacts.getAllContacts)
+
+  if (isLoading) {
+    return(
+      <div className='container mx-auto py-12'>
+        <BarLoader width={"100%"} color='#36d7b7'/>
+      </div>
+    )
+  }
+  
+
   return (
     <div>ConatctPage</div>
   )
